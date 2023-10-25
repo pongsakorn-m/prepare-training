7f32cfd582004d84026a78e05c15d882ef7f546a
pongsakorn_m <pongsakorn_m@softsquaregroup.com>
2023-10-25 18:03:52 +0700

    add feature 3

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function feature2() {
     alert('new changes from feature 2. change from first person.');
     alert('new changes from feature 2. change from second person.');
 }
+
+function feature3() {
+    // from branch release
+    alert('from feature 3.');
+}
